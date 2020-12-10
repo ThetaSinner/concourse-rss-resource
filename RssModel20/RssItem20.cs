648ef1fbc7779170f25a623fa904d5186373f64b
ThetaSinner <greatdjonfire@hotmail.co.uk>
2020-12-10 14:07:08 +0000

    Capture all RSS item fields and update the feed to trigger a new build

--- RssModel20/RssItem20.cs
+++ RssModel20/RssItem20.cs
@@ -10,5 +10,68 @@ namespace concourse_rss_resource.RssModel20
         
         [XmlElement(ElementName = "link")]
         public string Link { get; set; }
+        
+        [XmlElement(ElementName = "description")]
+        public string Description { get; set; }
+        
+        [XmlElement(ElementName = "author")]
+        public string Author { get; set; }
+        
+        [XmlElement(ElementName = "category")]
+        public Category Category { get; set; }
+        
+        [XmlElement(ElementName = "comments")]
+        public string Comments { get; set; }
+        
+        [XmlElement(ElementName = "enclosure")]
+        public Enclosure Enclosure { get; set; }
+        
+        [XmlElement(ElementName = "guid")]
+        public Guid Guid { get; set; }
+        
+        [XmlElement(ElementName = "pubDate")]
+        public string PubDate { get; set; }
+        
+        [XmlElement(ElementName = "source")]
+        public Source Source { get; set; }
+    }
+
+    public class Source
+    {
+        [XmlText]
+        public string Value { get; set; }
+        
+        [XmlAttribute("url")]
+        public string Url { get; set; }
+    }
+
+    public class Enclosure
+    {
+        [XmlAttribute("url")]
+        public string Url { get; set; }
+        
+        [XmlAttribute("length")]
+        public string Length { get; set; }
+        
+        [XmlAttribute("type")]
+        public string Type { get; set; }
+    }
+    
+    public class Category
+    {
+        [XmlText]
+        public string Value { get; set; }
+        
+        [XmlAttribute("domain")]
+        public string Domain { get; set; }
+    }
+    
+    public class Guid
+    {
+        [XmlText]
+        public string Value { get; set; }
+        
+        [XmlAttribute("isPermaLink")]
+        public string IsPermaLink { get; set; }
     }
 }
